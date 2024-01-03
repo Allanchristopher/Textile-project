74b543e4f74b448cb81a8aafee356f658fdabaec
Allanchristopher <allanchristopher2516@gmail.com>
2024-01-03 17:36:40 +0530

    Creating Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Login from './Components/Login';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
-     <Login/>
+     <Home/>
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Home.css";
+import { Layout} from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+function Home() {
+    
+  return (
+    <div className='Home'>
+
+    </div>
+  )
+}
+
+export default Home
